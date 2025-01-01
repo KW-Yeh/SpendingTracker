66f5ed291726f1930cdf0a0716996c343b7a9cab
KW <a0979597291@gmail.com>
2025-01-01 12:13:04 +0800

    Fix wrong number

--- my-app/src/composites/EditorBlock.tsx
+++ my-app/src/composites/EditorBlock.tsx
@@ -1,189 +1,189 @@
-'use client';
+"use client";
 
-import { EnterIcon } from '@/components/icons/EnterIcon';
-import { Loading } from '@/components/icons/Loading';
-import { Modal } from '@/components/Modal';
-import { NumberKeyboard } from '@/components/NumberKeyboard';
-import { Select } from '@/components/Select';
-import { useGetSpendingCtx } from '@/context/SpendingProvider';
-import { putItem } from '@/services/dbHandler';
+import { EnterIcon } from "@/components/icons/EnterIcon";
+import { Loading } from "@/components/icons/Loading";
+import { Modal } from "@/components/Modal";
+import { NumberKeyboard } from "@/components/NumberKeyboard";
+import { Select } from "@/components/Select";
+import { useGetSpendingCtx } from "@/context/SpendingProvider";
+import { putItem } from "@/services/dbHandler";
 import {
-  INCOME_TYPE_OPTIONS,
-  Necessity,
-  OUTCOME_TYPE_OPTIONS,
-  SpendingType,
-} from '@/utils/constants';
-import { normalizeNumber } from '@/utils/normalizeNumber';
-import { useSession } from 'next-auth/react';
+	INCOME_TYPE_OPTIONS,
+	Necessity,
+	OUTCOME_TYPE_OPTIONS,
+	SpendingType,
+} from "@/utils/constants";
+import { normalizeNumber } from "@/utils/normalizeNumber";
+import { useSession } from "next-auth/react";
 import {
-  ChangeEvent,
-  FormEvent,
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
-} from 'react';
-import { v4 as uuid } from 'uuid';
+	ChangeEvent,
+	FormEvent,
+	useCallback,
+	useEffect,
+	useRef,
+	useState,
+} from "react";
+import { v4 as uuid } from "uuid";
 
 interface Props {
-  type: SpendingType;
-  date: Date;
-  data?: SpendingRecord;
+	type: SpendingType;
+	date: Date;
+	data?: SpendingRecord;
 }
 
 export const EditorBlock = (props: Props) => {
-  const { syncData } = useGetSpendingCtx();
-  const { data: session } = useSession();
-  const spendingCategories =
-    props.type === SpendingType.Income
-      ? INCOME_TYPE_OPTIONS
-      : OUTCOME_TYPE_OPTIONS;
-  const [loading, setLoading] = useState(false);
-  const [amount, setAmount] = useState(props.data?.amount ?? 0);
-  const [selectedNecessity, setSelectedNecessity] = useState(
-    props.data?.necessity,
-  );
-  const [selectedCategory, setSelectedCategory] = useState(
-    props.data?.category,
-  );
-  const [newDesc, setNewDesc] = useState(props.data?.description ?? '');
-  const modalRef = useRef<ModalRef>(null);
+	const {syncData} = useGetSpendingCtx();
+	const {data: session} = useSession();
+	const spendingCategories =
+		props.type === SpendingType.Income
+			? INCOME_TYPE_OPTIONS
+			: OUTCOME_TYPE_OPTIONS;
+	const [loading, setLoading] = useState(false);
+	const [amount, setAmount] = useState(props.data?.amount ?? 0);
+	const [selectedNecessity, setSelectedNecessity] = useState(
+		props.data?.necessity,
+	);
+	const [selectedCategory, setSelectedCategory] = useState(
+		props.data?.category,
+	);
+	const [newDesc, setNewDesc] = useState(props.data?.description ?? "");
+	const modalRef = useRef<ModalRef>(null);
 
-  useEffect(() => {
-    setAmount(props.data?.amount ?? 0);
-    setNewDesc(props.data?.description ?? '');
-    setSelectedNecessity(props.data?.necessity);
-    setSelectedCategory(props.data?.category);
-  }, [props.data]);
+	useEffect(() => {
+		setAmount(props.data?.amount ?? 0);
+		setNewDesc(props.data?.description ?? "");
+		setSelectedNecessity(props.data?.necessity);
+		setSelectedCategory(props.data?.category);
+	}, [props.data]);
 
-  const handleEditDesc = (event: ChangeEvent<HTMLInputElement>) => {
-    setNewDesc(event.target.value);
-  };
+	const handleEditDesc = (event: ChangeEvent<HTMLInputElement>) => {
+		setNewDesc(event.target.value);
+	};
 
-  const handleSelectNecessity = (value: string) => {
-    setSelectedNecessity(value);
-  };
+	const handleSelectNecessity = (value: string) => {
+		setSelectedNecessity(value);
+	};
 
-  const handleSelectCategory = (value: string) => {
-    setSelectedCategory(value);
-  };
+	const handleSelectCategory = (value: string) => {
+		setSelectedCategory(value);
+	};
 
-  const handleOnSubmit = useCallback(
-    (event: FormEvent) => {
-      event.preventDefault();
-      const userEmail = session?.user?.email ?? 'test';
-      const formElement = event.target as HTMLFormElement;
-      const formData = new FormData(formElement);
-      const necessity = formData.get('necessity') as Necessity;
-      const category = formData.get('category') as string;
-      const description = formData.get('description') as string;
-      const amount = Math.abs(parseInt(formData.get('amount') as string));
-      if (amount === 0) return;
-      setLoading(true);
-      const newSpending: SpendingRecord = {
-        ...(props.data ?? {}),
-        id: props.data?.id ?? uuid(),
-        'user-token': userEmail,
-        type: props.type,
-        date: props.date.toUTCString(),
-        necessity,
-        category,
-        description,
-        amount,
-      };
+	const handleOnSubmit = useCallback(
+		(event: FormEvent) => {
+			event.preventDefault();
+			const userEmail = session?.user?.email ?? "test";
+			const formElement = event.target as HTMLFormElement;
+			const formData = new FormData(formElement);
+			const necessity = formData.get("necessity") as Necessity;
+			const category = formData.get("category") as string;
+			const description = formData.get("description") as string;
+			const amount = Math.abs(parseInt(formData.get("amount") as string));
+			if (amount === 0) return;
+			setLoading(true);
+			const newSpending: SpendingRecord = {
+				...(props.data ?? {}),
+				id: props.data?.id ?? uuid(),
+				"user-token": userEmail,
+				type: props.type,
+				date: props.date.toUTCString(),
+				necessity,
+				category,
+				description,
+				amount,
+			};
 
-      putItem(newSpending)
-        .then(() => {
-          syncData(userEmail);
-        })
-        .then(() => {
-          setLoading(false);
-          setNewDesc('');
-          setSelectedNecessity(undefined);
-          setSelectedCategory(undefined);
-          setAmount(0);
-        });
-    },
-    [session, props.type, props.date, props.data, syncData],
-  );
+			putItem(newSpending)
+				.then(() => {
+					syncData(userEmail);
+				})
+				.then(() => {
+					setLoading(false);
+					setNewDesc("");
+					setSelectedNecessity(undefined);
+					setSelectedCategory(undefined);
+					setAmount(0);
+				});
+		},
+		[session, props.type, props.date, props.data, syncData],
+	);
 
-  return (
-    <>
-      <form
-        onSubmit={handleOnSubmit}
-        className="flex h-fit w-full max-w-175 items-center divide-x divide-text rounded-lg border border-solid border-text"
-      >
-        <div className="flex h-10 flex-1 items-center text-xs sm:text-sm lg:text-base">
-          <Select
-            value={selectedNecessity ?? Necessity.Need}
-            className="h-full py-2 pl-4"
-            name="necessity"
-            onChange={handleSelectNecessity}
-          >
-            <Select.Item value={Necessity.Need}>{Necessity.Need}</Select.Item>
-            <Select.Item value={Necessity.NotNeed}>
-              {Necessity.NotNeed}
-            </Select.Item>
-          </Select>
-          <Select
-            value={selectedCategory ?? spendingCategories[0]}
-            className="h-full py-2 pl-4"
-            name="category"
-            onChange={handleSelectCategory}
-          >
-            {spendingCategories.map((category) => (
-              <Select.Item key={category} value={category}>
-                {category}
-              </Select.Item>
-            ))}
-          </Select>
-          <div className="h-full flex-1">
-            <input
-              type="text"
-              name="description"
-              value={newDesc}
-              onChange={handleEditDesc}
-              className="h-full w-full bg-transparent py-2 pl-4 pr-0 focus:outline-0"
-              placeholder="描述一下"
-            />
-          </div>
-          <button
-            type="button"
-            onClick={() => modalRef.current?.open()}
-            className="flex h-full w-16 items-center gap-1 p-2 sm:w-24"
-          >
-            <span>$</span>
-            <span>{amount}</span>
-            <input
-              type="text"
-              name="amount"
-              value={normalizeNumber(amount)}
-              className="hidden"
-              readOnly
-            />
-          </button>
-        </div>
-        <button
-          type="submit"
-          disabled={loading}
-          className="flex h-full w-12 shrink-0 items-center justify-center rounded-r-lg bg-primary-100 p-2 transition-colors hover:bg-primary-300 sm:w-12"
-        >
-          {loading && (
-            <Loading className="size-3 animate-spin text-white sm:size-4" />
-          )}
-          {!loading && <EnterIcon className="size-3 sm:size-4" />}
-        </button>
-      </form>
-      <Modal ref={modalRef} className="w-64 sm:w-80">
-        <h1 className="text-base font-bold sm:text-xl">Number Keyboard</h1>
-        <NumberKeyboard
-          default={amount}
-          onConfirm={(val) => {
-            setAmount(val);
-            modalRef.current?.close();
-          }}
-        />
-      </Modal>
-    </>
-  );
+	return (
+		<>
+			<form
+				onSubmit={handleOnSubmit}
+				className="flex h-fit w-full max-w-175 items-center divide-x divide-text rounded-lg border border-solid border-text"
+			>
+				<div className="flex h-10 flex-1 items-center text-xs sm:text-sm lg:text-base">
+					<Select
+						value={selectedNecessity ?? Necessity.Need}
+						className="h-full py-2 pl-4"
+						name="necessity"
+						onChange={handleSelectNecessity}
+					>
+						<Select.Item value={Necessity.Need}>{Necessity.Need}</Select.Item>
+						<Select.Item value={Necessity.NotNeed}>
+							{Necessity.NotNeed}
+						</Select.Item>
+					</Select>
+					<Select
+						value={selectedCategory ?? spendingCategories[0]}
+						className="h-full py-2 pl-4"
+						name="category"
+						onChange={handleSelectCategory}
+					>
+						{spendingCategories.map((category) => (
+							<Select.Item key={category} value={category}>
+								{category}
+							</Select.Item>
+						))}
+					</Select>
+					<div className="h-full flex-1">
+						<input
+							type="text"
+							name="description"
+							value={newDesc}
+							onChange={handleEditDesc}
+							className="h-full w-full bg-transparent py-2 pl-4 pr-0 focus:outline-0"
+							placeholder="描述一下"
+						/>
+					</div>
+					<button
+						type="button"
+						onClick={() => modalRef.current?.open()}
+						className="flex h-full w-16 items-center gap-1 p-2 sm:w-24"
+					>
+						<span>$</span>
+						<span>{normalizeNumber(amount)}</span>
+						<input
+							type="text"
+							name="amount"
+							value={amount}
+							className="hidden"
+							readOnly
+						/>
+					</button>
+				</div>
+				<button
+					type="submit"
+					disabled={loading}
+					className="flex h-full w-12 shrink-0 items-center justify-center rounded-r-lg bg-primary-100 p-2 transition-colors hover:bg-primary-300 sm:w-12"
+				>
+					{loading && (
+						<Loading className="size-3 animate-spin text-white sm:size-4"/>
+					)}
+					{!loading && <EnterIcon className="size-3 sm:size-4"/>}
+				</button>
+			</form>
+			<Modal ref={modalRef} className="w-64 sm:w-80">
+				<h1 className="text-base font-bold sm:text-xl">Number Keyboard</h1>
+				<NumberKeyboard
+					default={amount}
+					onConfirm={(val) => {
+						setAmount(val);
+						modalRef.current?.close();
+					}}
+				/>
+			</Modal>
+		</>
+	);
 };
