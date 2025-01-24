468a9db8fcf6af709e75924e4a57b1fe1a1a36cf
kw_yeh <kw_yeh@cyberlink.com>
2025-01-24 13:15:40 +0800

    [what] Refine UI
    [how] as what
    
    [why] as what

--- my-app/src/app/list/CostTable.tsx
+++ my-app/src/app/list/CostTable.tsx
@@ -9,9 +9,19 @@ export const CostTable = ({
   total: number;
   list: PieChartDataItem[];
 }) => {
-  const largest = list.reduce((prev, current) => {
-    return prev.value > current.value ? prev : current;
-  });
+  const largest = list.reduce(
+    (prev, current) => {
+      return prev.value > current.value ? prev : current;
+    },
+    {
+      id: '',
+      name: '',
+      value: 0,
+      necessary: 0,
+      unnecessary: 0,
+      color: '',
+    },
+  );
   return (
     <div className="my-2 flex w-full flex-col gap-2 rounded-xl p-3 shadow">
       <h3 className="text-sm font-semibold">{title}</h3>
