057b83e31c957908ed40ae0ebeca605f2e5e4a11
kw_yeh <kw_yeh@cyberlink.com>
2025-02-25 09:56:26 +0800

    [what] Refine UI
    [how] as what
    
    [why] as what

--- my-app/src/composites/Caption.tsx
+++ my-app/src/composites/Caption.tsx
@@ -3,7 +3,7 @@ import { MenuButton } from '@/composites/MenuButton';
 
 export const Caption = ({ openAside }: { openAside: () => void }) => {
   return (
-    <div className="sticky left-0 top-0 z-40 w-full border-b border-solid border-text p-4 backdrop-blur sm:p-6">
+    <div className="sticky left-0 top-0 z-40 w-full p-4 shadow backdrop-blur sm:p-6">
       <div className="flex items-center">
         <MenuButton openAside={openAside} />
         <div className="flex flex-1 justify-center pr-8">
