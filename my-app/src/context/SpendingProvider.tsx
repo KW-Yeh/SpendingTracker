a96dbe400577de4e1d6939a7a66fa63cd083c6d6
KW <a0979597291@gmail.com>
2025-03-10 11:58:22 +0800

    [what] Improve UX
    [why] The latency of fetching data from AWS will damage the UX.
    
    [how] Use IndexedDB to retrieve data for the first time, and cache data after each fetch responses.

--- my-app/src/context/SpendingProvider.tsx
+++ my-app/src/context/SpendingProvider.tsx
@@ -1,12 +1,15 @@
 'use client';
 
+import { useIDB } from '@/hooks/useIDB';
 import { getItems } from '@/services/getRecords';
 import {
   createContext,
   ReactNode,
   useCallback,
   useContext,
+  useEffect,
   useMemo,
+  useRef,
   useState,
 } from 'react';
 
@@ -25,6 +28,8 @@ const INIT_CTX_VAL: {
 export const SpendingProvider = ({ children }: { children: ReactNode }) => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState<SpendingRecord[]>([]);
+  const { db: IDB, getData: getDataFromIDB, setData: setData2IDB } = useIDB();
+  const controllerRef = useRef<AbortController>(new AbortController());
 
   const handleState = useCallback((res: SpendingRecord[]) => {
     setData(res);
@@ -36,11 +41,21 @@ export const SpendingProvider = ({ children }: { children: ReactNode }) => {
       if (!email && !groupId) return;
       getItems(groupId, email, time)
         .then((res) => {
+          controllerRef.current.abort();
           handleState(res.data);
+          if (IDB) {
+            setData2IDB(IDB, res.data, time)
+              .then(() => {
+                console.log('Update IDB success.');
+              })
+              .catch((err) => {
+                console.log('Update IDB failed: ', err);
+              });
+          }
         })
         .catch(console.error);
     },
-    [handleState],
+    [IDB, handleState, setData2IDB],
   );
 
   const syncData = useCallback(
@@ -60,6 +75,23 @@ export const SpendingProvider = ({ children }: { children: ReactNode }) => {
     }),
     [loading, data, syncData, handleState],
   );
+
+  useEffect(() => {
+    const controller = (controllerRef.current = new AbortController());
+    if (IDB) {
+      console.log('Get Data from IDB');
+      getDataFromIDB(IDB, controller.signal)
+        .then((res) => {
+          if (res && res.length === 1) {
+            handleState(JSON.parse(res[0].data));
+          }
+        })
+        .catch((err) => {
+          console.log('Error while syncing data: ', err);
+        });
+    }
+    return () => controller.abort();
+  }, [IDB, getDataFromIDB, handleState]);
 
   return <Ctx.Provider value={ctxVal}>{children}</Ctx.Provider>;
 };
---
+++ my-app/src/hooks/useIDB.ts
@@ -0,0 +1,111 @@
+'use client';
+
+import { useCallback, useEffect, useState } from 'react';
+
+const DB_NAME = 'Expense Tracking';
+const DB_VERSION = 1;
+const STORE_NAME = 'Expense Record';
+
+interface DATA_IDB {
+  group: string;
+  year: string;
+  month: string;
+  data: string;
+}
+
+export const useIDB = () => {
+  const [db, setDb] = useState<IDBDatabase | null>(null);
+
+  useEffect(() => {
+    const openRequest = indexedDB.open(DB_NAME, DB_VERSION);
+
+    openRequest.onupgradeneeded = () => {
+      const db = openRequest.result;
+      if (!db.objectStoreNames.contains(STORE_NAME)) {
+        const store = db.createObjectStore(STORE_NAME, {
+          keyPath: 'id',
+          autoIncrement: true,
+        });
+        store.createIndex('year_month', ['year', 'month'], { unique: true });
+      }
+    };
+
+    openRequest.onsuccess = () => setDb(openRequest.result);
+    openRequest.onerror = () =>
+      console.error('Error opening IndexedDB:', openRequest.error);
+  }, []);
+
+  const setData = useCallback(
+    (
+      db: IDBDatabase,
+      record: SpendingRecord[],
+      time: string = new Date().toUTCString(),
+    ): Promise<void> => {
+      if (!db) return Promise.reject('Database not initialized');
+
+      return new Promise((resolve, reject) => {
+        const transaction = db.transaction(STORE_NAME, 'readwrite');
+        const store = transaction.objectStore(STORE_NAME);
+        const year = new Date(time).getFullYear();
+        const month = new Date(time).getMonth();
+
+        const index = store.index('year_month');
+        const checkRequest = index.get([year, month]);
+        checkRequest.onsuccess = () => {
+          if (!checkRequest.result) {
+            const request = store.add({
+              year,
+              month,
+              data: JSON.stringify(record),
+            });
+            request.onsuccess = () => resolve();
+            request.onerror = () => reject(request.error);
+          }
+        };
+      });
+    },
+    [],
+  );
+
+  const getData = useCallback(
+    (db: IDBDatabase, signal: AbortSignal): Promise<DATA_IDB[] | undefined> => {
+      if (!db) return Promise.reject('Database not initialized');
+
+      return new Promise((resolve, reject) => {
+        const transaction = db.transaction(STORE_NAME, 'readonly');
+        let isTransactionActive = true;
+
+        transaction.oncomplete = () => {
+          isTransactionActive = false;
+        };
+
+        transaction.onerror = (event) => {
+          isTransactionActive = false;
+          reject((event.target as IDBTransaction).error);
+        };
+
+        signal.addEventListener('abort', () => {
+          if (isTransactionActive) {
+            console.log('aborted');
+            transaction.abort();
+          }
+        });
+
+        const store = transaction.objectStore(STORE_NAME);
+        const index = store.index('year_month');
+        const year = new Date().getFullYear();
+        const month = new Date().getMonth();
+        const request = index.getAll([year, month]);
+
+        request.onsuccess = () =>
+          resolve(request.result as unknown as DATA_IDB[]);
+        request.onerror = () => reject(request.error);
+
+        transaction.onabort = () => reject('Transaction aborted');
+      });
+    },
+    [],
+  );
+
+  return { db, setData, getData };
+};
