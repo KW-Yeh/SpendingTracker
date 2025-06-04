cc4b55d89ae91346c8767fae7c0a5b546fdd5b59
KW <a0979597291@gmail.com>
2025-06-04 23:51:09 +0800

    [what] Update UI

--- my-app/src/app/list/YearMonthFilter.tsx
+++ my-app/src/app/list/YearMonthFilter.tsx
@@ -30,10 +30,10 @@ export const YearMonthFilter = (props: Props) => {
     <div
       className={`flex w-full items-center justify-center gap-2 ${className}`}
     >
-      <div className="border-text w-30 rounded-lg border border-solid bg-background px-4 py-2">
+      <div className="border-text bg-background w-30 rounded-lg border border-solid px-4 py-2">
         <Select
           name="year"
-          caretStyle="rounded-full bg-text size-3 p-0.5 text-background"
+          caretStyle="rounded-full bg-text size-4 p-1 text-background"
           className="w-full"
           value={year}
           onChange={setYear}
@@ -51,14 +51,14 @@ export const YearMonthFilter = (props: Props) => {
         </Select>
       </div>
 
-      <div className="border-text w-25 rounded-lg border border-solid bg-background px-4 py-2">
+      <div className="border-text bg-background w-25 rounded-lg border border-solid px-4 py-2">
         <Select
           name="month"
           value={month}
           label={month.padStart(2, '0')}
           onChange={setMonth}
           className="w-full"
-          caretStyle="rounded-full bg-text size-3 p-0.5 text-background"
+          caretStyle="rounded-full bg-text size-4 p-1 text-background"
         >
           {Array(12)
             .fill(0)
