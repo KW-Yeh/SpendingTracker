1de29882056234047c55fc0eaf264717cc4afa50
KW <a0979597291@gmail.com>
2025-12-13 16:31:46 +0800

    feat: integrate AWS Aurora DSQL connection and update dependencies
    - Added AWS SDK clients for DSQL and OIDC to package.json.
    - Implemented getDb utility to establish a connection to AWS Aurora using DSQL.
    - Updated Home component to query the current time from the database and display it.
    - Modified tsconfig.json to support new JSX transform and include additional type definitions.

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,5 +1,16 @@
-import { redirect, RedirectType } from 'next/navigation';
+import { getDb } from '@/utils/getAurora';
 
-export default function Home() {
-  redirect('/insert', RedirectType.replace);
+export default async function Home() {
+  const db = await getDb();
+  try {
+    const result = await db.query('SELECT NOW()');
+    return (
+      <h1>
+        Test AWS Aurora DSQL Connection:{' '}
+        {new Date(result.rows[0].now).toString()}
+      </h1>
+    );
+  } finally {
+    await db.end();
+  }
 }
---
+++ my-app/src/utils/getAurora.ts
@@ -0,0 +1,49 @@
+import 'server-only';
+import { awsCredentialsProvider } from '@vercel/functions/oidc';
+import { DsqlSigner } from '@aws-sdk/dsql-signer';
+import { Client } from 'pg';
+
+const {
+  AWS_ROLE_ARN,
+  AURORA_DSQL_HOST,
+  AURORA_DSQL_PORT,
+  AURORA_DSQL_REGION,
+  AURORA_DSQL_DB,
+  AURORA_DSQL_USER,
+} = process.env;
+
+async function getPassword() {
+  if (process.env.VERCEL) {
+    const creds = await awsCredentialsProvider({
+      roleArn: AWS_ROLE_ARN!,
+    });
+    const signer = new DsqlSigner({
+      hostname: AURORA_DSQL_HOST!,
+      region: AURORA_DSQL_REGION!,
+      credentials: creds,
+    });
+    return await signer.getDbConnectAdminAuthToken();
+  }
+
+  const signer = new DsqlSigner({
+    hostname: AURORA_DSQL_HOST!,
+    region: AURORA_DSQL_REGION!,
+  });
+  return await signer.getDbConnectAdminAuthToken();
+}
+
+export async function getDb() {
+  const password = await getPassword();
+
+  const client = new Client({
+    host: AURORA_DSQL_HOST!,
+    port: Number(AURORA_DSQL_PORT ?? 5432),
+    user: AURORA_DSQL_USER!,
+    password,
+    database: AURORA_DSQL_DB!,
+    ssl: { rejectUnauthorized: true },
+  });
+
+  await client.connect();
+  return client;
+}
